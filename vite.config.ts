23e9804c0395d630a299c8ecd27544d9cdf315e1
tirtana888 <bisnis.ariftirtana@gmail.com>
2025-11-30 15:33:06 +0700

    feat: Configure production build to use serve

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Konfigurasi untuk development lokal
     host: '0.0.0.0',
     port: 8080,
-  },
-  preview: {
-    // Konfigurasi KRUSIAL untuk Cloud Run / Production Preview
-    // Mengizinkan akses dari luar container (0.0.0.0)
-    host: '0.0.0.0',
-    // Memaksa berjalan di port 8080 sesuai standar Cloud Run
-    port: 8080,
-    // Mengizinkan semua host (penting untuk domain dinamis Cloud Run)
-    allowedHosts: true,
-  },
+  }
 })
