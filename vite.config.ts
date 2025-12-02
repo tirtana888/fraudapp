164148fa400b1c51dc5426d5ff2b6555eb3cb1d3
tirtana888 <bisnis.ariftirtana@gmail.com>
2025-12-02 15:41:12 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false,
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      sourcemap: false,
+      rollupOptions: {
+        input: resolve(__dirname, isDev ? 'index.dev.html' : 'index.html')
+      }
+    }
   }
 })
